0cae4187638a7a29278709f42382685b1e02f31b
Jkz1 <jjikky3@gmail.com>
2025-10-31 10:57:12 +0700

    Halfway doing filter

--- lib/home.dart
+++ lib/home.dart
@@ -82,6 +82,52 @@ class _HomePageState extends State<HomePage> {
     super.dispose();
   }
 
+  final List<String> filtersAnime = [
+    'Airing',
+    'Upcoming',
+    'ByPopularity',
+    'Favorite',
+  ];
+  final List<String> ratingsAnime = ["G", "PG", "PG-13", "R", "R+", "Rx"];
+
+  final List<String> typeManga = [
+    "manga",
+    "novel",
+    "lightnovel",
+    "oneshot",
+    "doujin",
+    "manhwa",
+    "manhua"
+  ];
+
+  final List<String> filterManga = [
+    "publishing",
+    "upcoming",
+    "bypopularity",
+    "favorite"
+  ];
+
+  String? selectedFilter;
+  String? selectedRating;
+
+  void resetFilters() {
+    setState(() {
+      selectedFilter = null;
+      selectedRating = null;
+    });
+  }
+
+  void applyFilters() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          'Filter diterapkan:\nFilter: ${selectedFilter ?? "-"} | Rating: ${selectedRating ?? "-"}',
+        ),
+        duration: const Duration(seconds: 2),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -137,6 +183,212 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           SliverToBoxAdapter(child: Container(height: 5, color: Colors.orange)),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              const SizedBox(height: 8),
+
+              ExpansionTile(
+  leading: const Icon(Icons.filter_list, color: Color(0xFFEFEEEA)),
+  title: const Text(
+    'Filter & Rating',
+    style: TextStyle(
+      color: Color(0xFFEFEEEA),
+      fontWeight: FontWeight.w600,
+    ),
+  ),
+  backgroundColor: const Color(0xFF273F4F),
+  collapsedBackgroundColor: const Color(0xFF273F4F),
+  iconColor: const Color(0xFFEFEEEA),
+  collapsedIconColor: const Color(0xFFEFEEEA),
+  children: [
+    Container(
+      color: const Color(0xFF273F4F),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // 🌿 Filter
+          const Text(
+            "Filter",
+            style: TextStyle(
+              color: Color(0xFFEFEEEA),
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Wrap(
+            spacing: 8,
+            runSpacing: 8,
+            children: filtersAnime.map((item) {
+              final isSelected = selectedFilter == item;
+              return ChoiceChip(
+                label: Text(
+                  item,
+                  style: TextStyle(
+                    color: isSelected
+                        ? const Color(0xFF273F4F)
+                        : const Color(0xFFEFEEEA),
+                    fontWeight:
+                        isSelected ? FontWeight.bold : FontWeight.normal,
+                  ),
+                ),
+                selected: isSelected,
+                backgroundColor: const Color(0xFF273F4F),
+                selectedColor: const Color(0xFFEFEEEA),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: BorderSide(
+                    color: isSelected
+                        ? const Color(0xFFEFEEEA)
+                        : Colors.grey.shade600,
+                  ),
+                ),
+                onSelected: (selected) {
+                  setState(() {
+                    selectedFilter = selected ? item : null;
+                  });
+                },
+              );
+            }).toList(),
+          ),
+
+          const SizedBox(height: 20),
+
+          // 🌟 Rating
+          const Text(
+            "Rating",
+            style: TextStyle(
+              color: Color(0xFFEFEEEA),
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Wrap(
+            spacing: 8,
+            runSpacing: 8,
+            children: _currIndex == 0 ? 
+            ratingsAnime.map((rating) {
+              final isSelected = selectedRating == rating;
+              return ChoiceChip(
+                label: Text(
+                  rating,
+                  style: TextStyle(
+                    color: isSelected
+                        ? const Color(0xFF273F4F)
+                        : const Color(0xFFEFEEEA),
+                    fontWeight:
+                        isSelected ? FontWeight.bold : FontWeight.normal,
+                  ),
+                ),
+                selected: isSelected,
+                backgroundColor: const Color(0xFF273F4F),
+                selectedColor: const Color(0xFFEFEEEA),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: BorderSide(
+                    color: isSelected
+                        ? const Color(0xFFEFEEEA)
+                        : Colors.grey.shade600,
+                  ),
+                ),
+                onSelected: (selected) {
+                  setState(() {
+                    selectedRating = selected ? rating : null;
+                  });
+                },
+              );
+            }).toList() : typeManga.map((type) {
+              final isSelected = selectedRating == type;
+              return ChoiceChip(
+                label: Text(
+                  type,
+                  style: TextStyle(
+                    color: isSelected
+                        ? const Color(0xFF273F4F)
+                        : const Color(0xFFEFEEEA),
+                    fontWeight:
+                        isSelected ? FontWeight.bold : FontWeight.normal,
+                  ),
+                ),
+                selected: isSelected,
+                backgroundColor: const Color(0xFF273F4F),
+                selectedColor: const Color(0xFFEFEEEA),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: BorderSide(
+                    color: isSelected
+                        ? const Color(0xFFEFEEEA)
+                        : Colors.grey.shade600,
+                  ),
+                ),
+                onSelected: (selected) {
+                  setState(() {
+                    selectedRating = selected ? type : null;
+                  });
+                },
+              );
+            }).toList(),
+          ),
+
+          const SizedBox(height: 24),
+
+          // 🧭 Tombol Apply & Reset
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              // Tombol Reset (outline)
+              OutlinedButton(
+                onPressed: resetFilters,
+                style: OutlinedButton.styleFrom(
+                  side: const BorderSide(color: Color(0xFFEFEEEA), width: 1.3),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20, vertical: 12),
+                ),
+                child: const Text(
+                  "Reset",
+                  style: TextStyle(
+                    color: Color(0xFFEFEEEA),
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 12),
+              // Tombol Apply (oranye modern)
+              ElevatedButton(
+                onPressed: applyFilters,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFFF9F45),
+                  foregroundColor: Color(0xFF273F4F),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 22, vertical: 12),
+                  elevation: 0,
+                ),
+                child: const Text(
+                  "Apply",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
+    ),
+  ],
+)
+
+            ]),
+          ),
           SliverPadding(
             padding: const EdgeInsets.all(10),
             sliver:
